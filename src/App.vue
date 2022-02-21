cdac25816fd290a4af2f8e008c97f4d12f5925f1
damehra <damehra@gmail.com>
2022-02-21 08:04:13 +0530

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<HelloWorld msg="Hello World" />
+<HelloWorld msg="My name is nirav" />
+<HomeComp />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import HomeComp from './components/HomeComp.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        HelloWorld,
+        HomeComp,
+    }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
---
+++ src/components/HomeComp.vue
@@ -0,0 +1,32 @@
+<template>
+    <h1>My Email : {{email}}</h1>
+    <h1>My Mobile : {{mobile}}</h1>
+    <h1>My Address : {{getAddress()}}</h1>
+    <h1>My Country : {{setCountry('India')}}</h1>
+    <h1>My pincode : {{ pincode = 383001 }}</h1>
+    <h1>My pincode is 383001: {{ pincode == 383001 }}</h1>
+    <h1>My pincode is himatnagar: {{ city == 'himatnagar' }}</h1>
+</template>
+<script>
+export default {
+    name:'HomeComp',
+    data(){
+        return{
+            email : 'nirav@mail.com',
+            mobile : 9876543210,
+            city: 'himatnagar',
+            getAddress:function(){
+                return this.city+',Gujarat'
+            },
+            setCountry : function(country){
+                return country
+            }
+        }
+    }
+}
+</script>
+<style scoped>
+    h1{
+        color: orange;
+    }
+</style>
