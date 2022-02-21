d585d62b011f0ce6f71db713027c759dda28e33f
niravbhoi <niravbhoi007@gmail.com>
2022-02-21 13:17:55 +0530

    data using methods

--- src/components/HomeComp.vue
+++ src/components/HomeComp.vue
@@ -6,6 +6,11 @@
     <h1>My pincode : {{ pincode = 383001 }}</h1>
     <h1>My pincode is 383001: {{ pincode == 383001 }}</h1>
     <h1>My pincode is himatnagar: {{ city == 'himatnagar' }}</h1>
+
+    <h1>Data from method:</h1>
+    <h1>Name : {{ getName() }}</h1>
+    <h1>Email : {{ getData().email }}</h1>
+    <h1>Mobile : {{ getData().mobile }}</h1>
 </template>
 <script>
 export default {
@@ -20,6 +25,17 @@ export default {
             },
             setCountry : function(country){
                 return country
+            }
+        }
+    },
+    methods:{
+        getName(){
+            return "nirav";
+        },
+        getData(){
+            return {
+                email : "nirav9999@mail.com",
+                mobile : "989898988"
             }
         }
     }
