feba1a2a44302adb80460030dc943a97760ff0f3
damehra <damehra@gmail.com>
2022-02-22 08:40:15 +0530

    get input values

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,30 @@
-// <HelloWorld msg="Hello World" />
-// <HelloWorld msg="My name is nirav" />
-// <HomeComp />
 <template>
-    <TwoWay />
+    <h1>Hello</h1>
+    <!-- <HelloWorld msg="Hello World" />
+    <HelloWorld msg="My name is nirav" />
+    <HomeComp />
+    <TwoWay /> -->
+    <GetInput />
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 // import HomeComp from './components/HomeComp.vue'
-import TwoWay from './components/TwoWay.vue'
+// import TwoWay from './components/TwoWay.vue'
+import GetInput from './components/GetInput.vue'
 
 export default {
     name: 'App',
+    data(){
+        return {
+
+        }
+    },
     components: {
         // HelloWorld,
         // HomeComp,
-        TwoWay,
+        // TwoWay,
+        GetInput
     }
 }
 </script>
---
+++ src/components/GetInput.vue
@@ -0,0 +1,28 @@
+<template>
+    <h1>Get Input value</h1>
+    <label for="email">Email</label>
+    <input type="email" id="email" v-model="email">
+    <br>
+    <br>
+    <label for="password">Password</label>
+    <input type="password" id="password" v-model="password">
+    <br>
+    <br>
+    <button v-on:click="getData()">Get Data</button>
+</template>
+<script>
+export default {
+    name : "GetInput",
+    data() {
+        return {
+            email : null,
+            password : null,
+        }
+    },
+    methods:{
+        getData(){
+            console.log("data:", this.email, this.password);
+        }
+    }
+}
+</script>
